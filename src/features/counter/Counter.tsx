795bc2542042638e08e9ea1284f7100a10d9c7f5
leenawat papahom <leenawat.pa@gmail.com>
2024-01-05 19:50:01 +0700

    finish

---
+++ src/features/counter/Counter.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import type { RootState } from '../../app/store'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment, incrementByAmount } from './counterSlice'
+
+export function Counter() {
+  const count = useSelector((state: RootState) => state.counter.value)
+  const dispatch = useDispatch()
+
+  return (
+    <div>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <button
+          aria-label="Increment value by amount"
+          onClick={() => dispatch(incrementByAmount(2))}
+        >
+          Increment by amount
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch({type: 'counter/decrement'})}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  )
+}
